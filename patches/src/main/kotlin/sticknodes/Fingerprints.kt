2de70d9c44b7f706d846918d4355b489367cfc0c
HZbutcoding <hzalt444@gmail.com>
2025-10-03 15:42:56 +0300

    feat: fixed pathing: missing Opcode

--- patches/src/main/kotlin/sticknodes/Fingerprints.kt
+++ patches/src/main/kotlin/sticknodes/Fingerprints.kt
@@ -9,15 +9,16 @@ val figureFiltersInitFingerprint = fingerprint {
     returns("V")
     parameters("Lcom/badlogic/gdx/graphics/g2d/TextureAtlas;", "Lcom/badlogic/gdx/graphics/g2d/TextureAtlas;", "Lcom/badlogic/gdx/scenes/scene2d/utils/Drawable;")
     opcodes(
+        Opcode.INVOKE_SUPER,
         Opcode.NEW_INSTANCE,
         Opcode.INVOKE_DIRECT,
         Opcode.IPUT_OBJECT,
         Opcode.INVOKE_VIRTUAL,
-        Opcode.IF_LT
+        Opcode.IF_GE
     )
     custom { method, classDef ->
         classDef.type == "Lorg/fortheloss/sticknodes/animationscreen/modules/tooltables/FigureFiltersToolTable;" &&
-                method.name == "<init>"
+                method.name == "initialize"
     }
 }
 
